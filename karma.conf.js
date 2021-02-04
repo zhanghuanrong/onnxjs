fd40232293ee8ba1e713614e69ada7ba9ddbcb3b
Lei Zhang <zhalei@microsoft.com>
2021-02-04 02:01:49 -0800

    work around WSL network interface.

--- karma.conf.js
+++ karma.conf.js
@@ -17,8 +17,8 @@ function getMachineIpAddress() {
 
   for (const ifname in ifaces) {
     for (const iface of ifaces[ifname]) {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
-        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      if ('IPv4' !== iface.family || iface.internal !== false || ifname.startsWith('vEthernet')) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses, and WSL used virtual interface
         continue;
       }
 
