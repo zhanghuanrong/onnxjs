19d5500bb705c07878af860ab01fbc1cdff47f67
NORTHAMERICA\ziyl <ziyue.liu7@gmail.com>
2021-02-04 17:22:28 -0800

    add relu and conv

---
+++ lib/backends/wasm/ops-vnext/conv.ts
@@ -0,0 +1,25 @@
+import {onnx} from 'onnx-proto';
+
+import {Attribute} from '../../../attribute';
+import {OpSet} from '../../../opset';
+import {OperatorInfo} from '../op-vnext';
+
+export class Conv implements OperatorInfo {
+  constructor(public opset: OpSet) {}
+
+  inferenceType(inputTypes: readonly onnx.TensorProto.DataType[]): onnx.TensorProto.DataType[] {
+    return [inputTypes[0]];
+  }
+
+  initializeAttributes(attribute: Attribute): void {
+    attribute.set('auto_pad', 'string', attribute.getString('auto_pad', 'NOTSET'));
+    // attribute.set('dilations ', 'ints', attribute.getInts('dilations', 1));
+    attribute.set('group', 'int', attribute.getInt('group', 1));
+    // attribute.set('kernel_shape', 'ints', attribute.getFloat('kernel_shape', 0));
+    // attribute.set('pads', 'string', attribute.getString('nearest_mode', 'round_prefer_floor'));
+  }
+
+  get hash() {
+    return '';
+  }
+}
